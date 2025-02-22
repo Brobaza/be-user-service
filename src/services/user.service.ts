ec5fb54c1ff8ccd6a9c5cf6d0989c133970ffc4c
ki3nAMK <123nguyenbahoangkien123@gmail.com>
2025-02-22 23:05:28 +0700

    task/hoangkien/addGrpcMethod

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -13,4 +13,66 @@ export class UsersService extends BaseService<User> {
   ) {
     super(userRepo);
   }
+
+  async isTakenEmail(email: string) {
+    // const availableEmail = await this.cacheDomain
+    //   .getRedisClient()
+    //   .sismember(RedisKey.AVAILABLE_EMAILS, email);
+    // if (availableEmail) return false;
+
+    // const val = await this.cacheDomain
+    //   .getRedisClient()
+    //   .sismember(RedisKey.EMAILS, email);
+    // if (val) return true;
+
+    const count = await this.userRepo.countBy({ email });
+    if (count > 0) {
+      // await this.setTakenEmail(email);
+      return true;
+    }
+
+    // await this.setAvailableEmail(email);
+    return false;
+  }
+
+  async isTakenPhoneNumber(phoneNumber: string) {
+    // const availableEmail = await this.cacheDomain
+    //   .getRedisClient()
+    //   .sismember(RedisKey.AVAILABLE_EMAILS, email);
+    // if (availableEmail) return false;
+
+    // const val = await this.cacheDomain
+    //   .getRedisClient()
+    //   .sismember(RedisKey.EMAILS, email);
+    // if (val) return true;
+
+    const count = await this.userRepo.countBy({ phoneNumber });
+    if (count > 0) {
+      // await this.setTakenEmail(email);
+      return true;
+    }
+
+    // await this.setAvailableEmail(email);
+    return false;
+  }
+
+  async getUserByUsername(username: string) {
+    if (await this.isTakenEmail(username)) {
+      const user = await this.userRepo.findOne({
+        where: { email: username },
+      });
+
+      return user;
+    }
+
+    if (await this.isTakenPhoneNumber(username)) {
+      const user = await this.userRepo.findOne({
+        where: { phoneNumber: username },
+      });
+
+      return user;
+    }
+
+    return null;
+  }
 }
